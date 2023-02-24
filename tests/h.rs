cc69cc84bd6bdccdc9d1d0b5a285d25523d360e3
Sid Olofsson <sid.olofsson@volvocars.com>
2023-02-24 20:40:10 +0100

    Add 'h' 'H' and "help" tests
    Also prepare with some additional state introspection for future tests.

---
+++ tests/h.rs
@@ -0,0 +1,146 @@
+// Test 'h', 'H' and "help" command
+// (In that order)
+
+mod shared;
+use shared::fixtures::PrintTest;
+use shared::mock_ui::*;
+
+use shared::dummy_io::DummyIO;
+use add_ed::buffer::Buffer;
+use add_ed::ui::ScriptedUI;
+use add_ed::Ed;
+use add_ed::error_consts::{
+  SELECTION_EMPTY,
+  HELP_TEXT,
+};
+use std::collections::HashMap;
+
+// We have some tests without fixtures in here, as we shouldn't panic on error
+// and care about state.print_errors unlike all other fixtures.
+
+// Verify behaviour of 'h' command
+//
+// - Takes no index.
+// - Takes no flags or arguments.
+// - Doesn't modify state.selection.
+// - Doesn't set/unset unsaved.
+// - If an error has occured during current session:
+//   - Prints that error.
+//   - Else prints that no error has occured (not an error).
+
+// Test 'h' when no error
+#[test]
+fn help_noerror() {
+  PrintTest{
+    init_buffer: vec![],
+    init_clipboard: vec![],
+    init_filepath: "path",
+    command_input: vec!["h"],
+    expected_buffer: vec![],
+    expected_buffer_saved: true,
+    expected_selection: (1,0),
+    expected_clipboard: vec![],
+    expected_filepath: "path",
+    expected_prints: vec![
+      Print{
+        text: vec!["No errors recorded.".to_string()],
+        n: false,
+        l: false,
+      }
+    ],
+  }.run();
+}
+
+// Test 'h' when there is an error
+#[test]
+fn help() {
+  let mut io = DummyIO::new();
+  let mut buffer = Buffer::new();
+  buffer.set_saved();
+  let mut inner_ui = MockUI{ prints_history: Vec::new() };
+  let mut ui = ScriptedUI{
+    print_ui: Some(&mut inner_ui),
+    input: vec![
+      ",n",
+      "h",
+    ].iter().map(|x|{
+      let mut s = x.to_string();
+      s.push('\n');
+      s
+    }).collect(),
+  };
+  // Construct editor state and run
+  let mut ed = Ed::new(
+    &mut buffer,
+    &mut io,
+    "path".to_owned(),
+    HashMap::new(),
+    false,
+    false,
+  );
+  assert_eq!(ed.run_macro(&mut ui), Err(SELECTION_EMPTY));
+  ed.run_macro(&mut ui).expect("Error running test");
+  assert!(buffer.is_empty());
+  assert_eq!(
+    vec![
+      Print{
+        text: vec![SELECTION_EMPTY.to_string(),],
+        n: false,
+        l: false,
+      },
+    ],
+    inner_ui.prints_history,
+  );
+}
+
+#[test]
+fn help_toggle() {
+  let mut io = DummyIO::new();
+  let mut buffer = Buffer::new();
+  buffer.set_saved();
+  let mut ui = ScriptedUI{
+    print_ui: None,
+    input: vec![
+      "H",
+    ].iter().map(|x|{
+      let mut s = x.to_string();
+      s.push('\n');
+      s
+    }).collect(),
+  };
+  // Construct editor state and run
+  let mut ed = Ed::new(
+    &mut buffer,
+    &mut io,
+    "path".to_owned(),
+    HashMap::new(),
+    false,
+    false,
+  );
+  assert_eq!(ed.see_state().print_errors, true);
+  ed.run_macro(&mut ui).expect("Error running test");
+  assert_eq!(ed.see_state().print_errors, false);
+  assert!(buffer.is_empty());
+}
+
+#[test]
+fn help_text() {
+  PrintTest{
+    init_buffer: vec![],
+    init_clipboard: vec![],
+    init_filepath: "path",
+    command_input: vec!["help"],
+    expected_buffer: vec![],
+    expected_buffer_saved: true,
+    expected_selection: (1,0),
+    expected_clipboard: vec![],
+    expected_filepath: "path",
+    expected_prints: vec![
+      Print{
+        text: [HELP_TEXT].iter().map(|s| s.to_string()).collect(),
+        n: false,
+        l: false,
+      }
+    ],
+  }.run();
+}
