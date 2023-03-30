5e3ccaca8ed4d1ca4b4be5f7a3ea533833ce3a83
David Sid Olofsson <contact@sidju.se>
2023-03-30 17:50:55 +0200

    Add tests for default command

---
+++ tests/default_command.rs
@@ -0,0 +1,66 @@
+// Tests for default command
+
+mod shared;
+use shared::fixtures::{
+  PrintTest,
+};
+use shared::mock_ui::Print;
+
+// Verify behaviour of default command
+//
+// - Takes optional selection
+//   - If given prints selection
+//   - If not given prints line after state.selection.1
+// - Prints literally if state.l is set
+// - Prints numbered if state.n is set
+// - state.selection is set to printed selection
+// - Does not change unsaved
+
+// Normal case, just print next line
+#[test]
+fn default() {
+  PrintTest{
+    init_buffer: vec!["a","b","c","d"],
+    init_clipboard: vec![],
+    command_input: vec!["1,4"],
+    expected_selection: (1,4),
+    expected_buffer: vec!["a","b","c","d"],
+    expected_buffer_saved: true,
+    expected_clipboard: vec![],
+    expected_prints: vec![
+      Print{
+        text: vec![
+          "a\n".to_string(),
+          "b\n".to_string(),
+          "c\n".to_string(),
+          "d\n".to_string(),
+        ],
+        n: false,
+        l: false,
+      }
+    ],
+  }.run()
+}
+
+// Test default selection
+#[test]
+fn default_noselection() {
+  PrintTest{
+    init_buffer: vec!["a","b","c","d"],
+    init_clipboard: vec![],
+    command_input: vec![""],
+    expected_selection: (4,4),
+    expected_buffer: vec!["a","b","c","d"],
+    expected_buffer_saved: true,
+    expected_clipboard: vec![],
+    expected_prints: vec![
+      Print{
+        text: vec![
+          "d\n".to_string(),
+        ],
+        n: false,
+        l: false,
+      }
+    ],
+  }.run()
+}
